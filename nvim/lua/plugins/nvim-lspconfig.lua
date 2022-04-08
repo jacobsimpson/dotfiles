397c0b8bf24038247fd95b0763da837aa9850d5d
Jacob Simpson <jacob.simpson@gmail.com>
2022-04-08 12:30:28 -0700

    Use Telescope for some LSP interactions.

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -33,8 +33,10 @@ return function()
     vim.api.nvim_set_keymap('n', '<C-s>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
     vim.api.nvim_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
     vim.api.nvim_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    vim.api.nvim_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    -- vim.api.nvim_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    vim.api.nvim_set_keymap('n', 'gr', '<cmd>Telescope lsp_references<CR>', opts)
     vim.api.nvim_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+    vim.api.nvim_set_keymap('n', '<space>a', '<cmd>Telescope lsp_code_actions theme=cursor<CR>', opts)
 
     -- -- Use an on_attach function to only map the following keys after the language server
     -- -- attaches to the current buffer.
