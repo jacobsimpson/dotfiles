e266760c08475b7fd9ece9f513df9a7ff93539c9
Jacob Simpson <jacob.simpson@gmail.com>
2022-04-08 08:40:57 -0700

    Add some more key mappings to LSP integration.

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,5 +1,10 @@
 return function()
-    -- Nice example LSP configuration: https://raygervais.dev/articles/2021/3/neovim-lsp/
+    -- The plugin repo itself has some nice guidance: https://github.com/neovim/nvim-lspconfig
+    -- Useful example LSP configuration: https://raygervais.dev/articles/2021/3/neovim-lsp/
+
+    -- Frequently the diagnostic virtual text goes off the screen. Currently the guidance is to just
+    -- call vim.lsp.diagnostic.show_line_diagnostics(), though I've noticed
+    -- vim.diagnostic.open_float() does the trick too.
 
     -- Make the sign column always there, even when there isn't an error. By reserving the space for the
     -- column always, there is less flicker during refreshes or loads. Without reserving this space, the
@@ -14,27 +19,53 @@ return function()
       }
     })
 
-    -- Adding the autocommands in a group means that when this file gets reloaded, the group gets
-    -- cleared and autocommands are re-added. Makes the development process of reloading this file with
-    -- changes idempotent.
-    local group = vim.api.nvim_create_augroup("lsp-configuration", {clear = true})
-    vim.api.nvim_create_autocmd("VimEnter", {
-        group = group,
-        callback = function()
-            vim.schedule(
-                function()
-                    require'lspconfig'.gopls.setup{}
-                end
-            )
-        end
-    })
+    local opts = { noremap=true, silent=true }
 
-    -- Frequently the virtual text goes off the screen. Currently the guidance is to just call
-    -- vim.lsp.diagnostic.show_line_diagnostics()
+    vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+    vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+    vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+    vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
 
-    vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', {noremap = true})
-    vim.api.nvim_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', {noremap = true})
-    vim.api.nvim_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', {noremap = true})
-    vim.api.nvim_set_keymap('n', '<c-s>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', {noremap = true})
-    vim.api.nvim_set_keymap('n', 'K',  '<cmd>lua vim.lsp.buf.hover()<CR>', {noremap = true})
+    vim.api.nvim_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    vim.api.nvim_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    vim.api.nvim_set_keymap('n', '<C-s>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    vim.api.nvim_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    vim.api.nvim_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    vim.api.nvim_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    vim.api.nvim_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+
+    -- -- Use an on_attach function to only map the following keys after the language server
+    -- -- attaches to the current buffer.
+    -- -- NOTE: This seems like a good idea, but the on_attach function never fired, so the keys
+    -- -- never mapped.
+    -- local on_attach = function(client, bufnr)
+    --     print "Attaching LSP to a buffer..."
+    --     -- Enable completion triggered by <c-x><c-o>
+    --     --vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+    --
+    --     -- Mappings.
+    --     -- See `:help vim.lsp.*` for documentation on any of the below functions
+    --     vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    --     vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    --     vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    --     vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    --     vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-s>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    --     --vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    --     --vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    --     --vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    --     --vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    --     vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    --     vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    --     vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    --     vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+    -- end
+    --
+    -- As far as I can tell, these lines are not required. There is equivalent lines in the
+    -- lsp-config documentation, but when I leave these out, I don't see a difference.
+    --
+    -- For a while, I did have the gopls line in my configuration.
+    -- require'lspconfig'.gopls.setup{on_attach = on_attach}
+    -- require'lspconfig'.rust_analyzer.setup{on_attach = on_attach}
 end
