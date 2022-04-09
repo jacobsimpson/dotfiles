e55e63e2b24ebb9a93682d673b9d63a9b87b8fec
Jacob Simpson <jacob.simpson@gmail.com>
2022-04-09 12:33:50 -0700

    stylua formatting.

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,17 +1,17 @@
 return function()
-	require("telescope").setup({
-		defaults = {
-			-- Ctrl-/ in Telescope will show the current key mappings.
-			mappings = {
-				i = {
-					["<C-j>"] = "move_selection_next",
-					["<C-k>"] = "move_selection_previous",
-				},
-				n = {
-					["<C-c>"] = require("telescope.actions").close,
-				},
-			},
-		},
-		layout_strategy = "vertical",
-	})
+    require('telescope').setup({
+        defaults = {
+            -- Ctrl-/ in Telescope will show the current key mappings.
+            mappings = {
+                i = {
+                    ['<C-j>'] = 'move_selection_next',
+                    ['<C-k>'] = 'move_selection_previous',
+                },
+                n = {
+                    ['<C-c>'] = require('telescope.actions').close,
+                },
+            },
+        },
+        layout_strategy = 'vertical',
+    })
 end
