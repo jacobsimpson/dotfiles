da472985f16d32aa534a03477675608f6bd62d7f
Jacob Simpson <jacob.simpson@gmail.com>
2022-10-01 00:35:08 -0700

    Disable pigeon plugin initialization.

--- nvim/lua/plugins/pigeon.lua
+++ nvim/lua/plugins/pigeon.lua
@@ -1,9 +1,9 @@
 return function()
-    local group = vim.api.nvim_create_augroup('pigeon_initialization', { clear = true })
-    vim.api.nvim_create_autocmd('BufNewFile', {
-        group = group,
-        callback = function()
-            vim.opt.filetime = 'pigeon'
-        end,
-    })
+    -- local group = vim.api.nvim_create_augroup('pigeon_initialization', { clear = true })
+    -- vim.api.nvim_create_autocmd('BufNewFile', {
+    --     group = group,
+    --     callback = function()
+    --         vim.opt.filetime = 'pigeon'
+    --     end,
+    -- })
 end
