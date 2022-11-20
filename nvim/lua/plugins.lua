873d70620c4307785d64d3c67cc60b2e6757898e
Jacob Simpson <jacob.simpson@google.com>
2022-11-20 11:36:51 -0800

    Add the rust-tools plugin commented out, there were problems.

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -130,6 +130,25 @@ return require('packer').startup(function(use)
         end,
     }
 
+    -- I like the idea of this plugin, it looks very helpful and people like it, but it isn't
+    -- loading. When I enable it, it kills Rust highlighting.
+    --use({
+    --    'simrat39/rust-tools.nvim',
+    --    config = function ()
+    --        local rt = require('rust-tools')
+    --        rt.setup({
+    --          server = {
+    --            on_attach = function(_, bufnr)
+    --              -- Hover actions
+    --              vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+    --              -- Code action groups
+    --              vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    --            end,
+    --          },
+    --        })
+    --    end,
+    --})
+
     -- Snippet support. I've never been really good at using this, but it seems like a good idea.
     use({ 'dcampos/nvim-snippy', config = require('plugins.nvim-snippy') })
 
